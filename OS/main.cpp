68883eb04dc4c97c1143b3c8f1440c6c4d75c9b1
Shubhang Jyotirmay <57489798+shubhangjyotirmay@users.noreply.github.com>
2020-10-03 16:40:58 +0530

    Create main.cpp

---
+++ OS/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "gotoxy.cpp"
+#include "delay.cpp"
+#include "FCFS.cpp"
+#include "SJF.cpp"
+
+using namespace std;
+
+int main()
+{
+    //fcfsInputSame();
+    //fcfsMenu();
+    sjfInput();
+    return 0;
+}
