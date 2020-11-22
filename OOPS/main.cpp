13bc01318f96c2df5f07ea8931b17351feab58b2
Shubhang Jyotirmay <57489798+shubhangjyotirmay@users.noreply.github.com>
2020-11-22 15:55:34 +0530

    Update main.cpp

--- OOPS/main.cpp
+++ OOPS/main.cpp
@@ -1,44 +1,158 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <cwchar>
 #include "gotoxy.cpp"
 #include "delay.cpp"
 #include "hospital.cpp"
+#include "bank.cpp"
+#include "transport.cpp"
+#include "education.cpp"
 
 using namespace std;
 
-// function definitions
-void welcome();
+void welcome() {
+    system("cls");
+    gotoxy(35, 5);
+    cout << "DELHI TECHNOLOGICAL UNIVERSITY";
+    gotoxy(40, 6);
+    cout << "INNOVATIVE PROJECT";
+    gotoxy(34, 8);
+    cout << "OBJECT ORIENTED PROGRAMMING";
+    gotoxy(45, 9);
+    cout << "SE-203";
+    gotoxy(30, 11);
+    cout << "PROJECT TITLE : CITY MANAGEMENT SYSTEM";
+    gotoxy(30, 12);
+    cout << "MADE BY       : SHUBHANG JYOTIRMAY  2K19/SE/123";
+    gotoxy(46, 13);
+    cout << "UTKARSH JAIN  2K19/SE/139";
+    delay(1000);
+    int i, j = 0;
+    gotoxy(45, 17);
+    cout << "LOADING";
+    do {
+        for (i = 0; i < 3; i++) {
+        gotoxy(53 + i, 17);
+        cout << ".";
+        delay(600);
+        }
+        system("cls");
+        gotoxy(35, 5);
+        cout << "DELHI TECHNOLOGICAL UNIVERSITY";
+        gotoxy(40, 6);
+        cout << "INNOVATIVE PROJECT";
+        gotoxy(34, 8);
+        cout << "OBJECT ORIENTED PROGRAMMING";
+        gotoxy(45, 9);
+        cout << "SE-203";
+        gotoxy(30, 11);
+        cout << "PROJECT TITLE : CITY MANAGEMENT SYSTEM";
+        gotoxy(30, 12);
+        cout << "MADE BY       : SHUBHANG JYOTIRMAY  2K19/SE/123";
+        gotoxy(46, 13);
+        cout << "UTKARSH JAIN  2K19/SE/139";
+        gotoxy(45, 17);
+        cout << "LOADING";
+        j++;
+    } while (j < 3);
+    system("cls");
+}
 
 // main function
 int main()
 {
-    /*system("cls");
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(cfi);
+    cfi.nFont = 0;
+    cfi.dwFontSize.X = 0;
+    cfi.dwFontSize.Y = 26;
+    cfi.FontFamily = FF_DONTCARE;
+    cfi.FontWeight = FW_DEMIBOLD;
+    std::wcscpy(cfi.FaceName, L"Courier");
+    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+    system("color 59");
+
     welcome();
-    delay(2000);
-    system("cls");*/
-    cout << "HELLO" << endl;
-    delay(2000);
-    menuHosp();
+    int ch;
+    string s;
+    do {
+        system("cls");
+        gotoxy(40, 5);
+        cout << "LIST OF DEPARTMENTS";
+        gotoxy(38, 7);
+        cout << "1. HOSPITAL DEPARTMENT";
+        gotoxy(38, 8);
+        cout << "2. EDUCATION DEPARTMENT";
+        gotoxy(38, 9);
+        cout << "3. BANK DEPARTMENT";
+        gotoxy(38, 10);
+        cout << "4. TRANSPORT DEPARTMENT";
+        gotoxy(38, 11);
+        cout << "5. EXIT";
+        gotoxy(38, 13);
+        cout << "Select the department : ";
+        cin >> ch;
+        if (ch == 1) {
+            gotoxy(38, 15);
+            cout << "ENTER PASSWORD : ";
+            cin >> s;
+            if (s == "hospital") {
+                menuHosp();
+            }
+            else {
+                gotoxy(38, 16);
+                cout << "WRONG PASSWORD";
+                gotoxy(38, 18);
+                cout << "MOVING TO THE MAIN MENU IN 3 SEC";
+                delay(3000);
+            }
+        }
+        else if (ch == 2) {
+            gotoxy(38, 15);
+            cout << "ENTER PASSWORD : ";
+            cin >> s;
+            if (s == "education") {
+                menuEdu();
+            }
+            else {
+                gotoxy(38, 16);
+                cout << "WRONG PASSWORD";
+                gotoxy(38, 18);
+                cout << "MOVING TO THE MAIN MENU IN 3 SEC";
+                delay(3000);
+            }
+        }
+        else if (ch == 3) {
+            gotoxy(38, 15);
+            cout << "ENTER PASSWORD : ";
+            cin >> s;
+            if (s == "bank") {
+                menuBank();
+            }
+            else {
+                gotoxy(38, 16);
+                cout << "WRONG PASSWORD";
+                gotoxy(38, 18);
+                cout << "MOVING TO THE MAIN MENU IN 3 SEC";
+                delay(3000);
+            }
+        }
+        else if (ch == 4) {
+            gotoxy(38, 15);
+            cout << "ENTER PASSWORD : ";
+            cin >> s;
+            if (s == "transport") {
+                menuTrans();
+            }
+            else {
+                gotoxy(38, 16);
+                cout << "WRONG PASSWORD";
+                gotoxy(38, 18);
+                cout << "MOVING TO THE MAIN MENU IN 3 SEC";
+                delay(3000);
+            }
+        }
+    } while (ch != 5);
     return 0;
 }
-
-/*void welcome() {
-    system("cls");
-    gotoxy(60, 9);
-    cout << "WELCOME";
-    gotoxy(44, 11);
-    cout << "Subject: Object Oriented Programming";
-    gotoxy(60, 12);
-    cout << "SE-203";
-    gotoxy(44, 15);
-    cout << "Project Title : City Management System";
-    gotoxy(50, 17);
-    cout << "Made By : Shubhang Jyotirmay";
-    gotoxy(60, 18);
-    cout << "2K19/SE/123";
-    gotoxy(60, 19);
-    cout << "Utkarsh Jain";
-    gotoxy(60, 20);
-    cout << "2K19/SE/139";
-}*/
